5daa821588aff1014cebc844e4f35b1dce583f93
Kamal Nayan <89720132+saka015@users.noreply.github.com>
2023-07-19 12:26:46 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div className='flex mb-12'>
 
     <div className="main my-40 mx-12">
-        <h1 className='text-6xl text-slate-800 font-bold my-0'>Eatt.Sleep.Code</h1>
+        <h1 className='text-6xl text-slate-800 font-bold my-0'>Eat.Sleep.Code</h1>
         <div className="button  my-8">
         <p className='text-lg  font-semibold break-words w-[600px]'>A developer team that is passionate about coding and technology. We are a group of experienced developers who are always looking for new challenges </p>
             <Link to="/joinus"><button className='animate-pulse my-4 bg-slate-700 text-white px-4 py-2'>Join Us!</button></Link>
